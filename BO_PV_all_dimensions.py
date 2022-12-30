39362145387daa1138b07ed48a2d5f202030ccae
shauryaa09 <shauryaa09@gmail.com>
2022-12-30 22:57:53 +0530

    Update BO_PV_all_dimensions.py

--- BO_PV_all_dimensions.py
+++ BO_PV_all_dimensions.py
@@ -14,37 +14,18 @@ import time
 from scipy.stats import norm
 from sklearn.gaussian_process.kernels import ExpSineSquared
 
-# import sys
-#np.set_printoptions(threshold=sys.maxsize)
+
 ##training set
 # "N is no. of dimensions of the problem"
 print("Warning: Input only integers")
 print("Input the total number of layers")
 N = int(input())
-offset = []
-low_lim = np.zeros(N, dtype=int)
-upp_lim = []
-points = []
-# for i in range(N):
-#     print('Enter starting thickness in nm for layer '+str(i+1))
-#     nx = int(input())
-#     print('Enter ending thickness in nm for layer '+ str(i+1))
-#     mx = int(input())
-#     offset.append(nx)
-#     upp_lim.append(mx)
-#     pt=int((mx-nx+1)/5)
-#     points.append(pt)
 
-# df_n=pandas.DataFrame()
-# df_n[0]=low_lim
-# df_n[1]=upp_lim
-# df_n[2]=points
-# df_n[3]=offset
-
+##Reading the upper limit and lower limit thicknesses of the layers from the csv file and also the total number of points one wants in the scanning or testing dataset
 df_n=pandas.read_csv('data_frame_tandem.csv')
 df_n.columns=['start','end','total points']
 
-##training set
+##initial set - just one point
 X=np.empty(N,dtype=object)
 for i in range(N):
     X[i]=np.random.randint(df_n.iloc[i,0],df_n.iloc[i,1],1).astype(int)
@@ -53,15 +34,15 @@ X[8]=X[8]*10000 # for c_si it only makes sense experimentally to have multiples 
 #Copying the set
 X_new=X.astype(int)
 
+##Now making a dataframe of the first point with each layer as a column
 df=pandas.DataFrame()
 for i in range(N):
     df[i]=X[i]
 
 
-##scanning space set
 
-# print(tracemalloc.get_traced_memory())
-##preprocessing
+
+##preprocessing - a method to treat data in order to make the program faster where one converts the set of numbers into a normal distribution
 df=df.astype(float)
 
 for i in range(N):
@@ -70,7 +51,7 @@ for i in range(N):
     scaler_x = preprocessing.StandardScaler().fit(d)
     df[i] = scaler_x.transform(d)
 
-
+##scanning space set- this is a set of points which is used by GPR to fit and then the results of the fit are used by acquisition function to go to the next point
 def scan_set_gen():
     dftest=pandas.DataFrame()
     temp=np.empty(N, dtype=object)
@@ -81,6 +62,7 @@ def scan_set_gen():
         dftest[i]=temp[i].flatten()
     dftest = dftest.astype(float)
     scaler = np.empty(N, dtype=object)
+    #preprocessing
     for i in range(N):
         d = dftest[i].to_numpy()
         d = d.reshape(-1, 1)
@@ -88,12 +70,11 @@ def scan_set_gen():
         dftest[i] = scaler[i].transform(d)
     return dftest,scaler
 
-c=0
-iter=5000
+c=0  # counter for convergence
+iter=5000 # total iterations
 maximum_tracker=np.zeros((iter))
 
 start_time = time.time()
-kernel = ExpSineSquared(length_scale=0.5, periodicity=1)
 
 Current_density_perovskite = []
 Current_density_silicon = []
@@ -103,10 +84,12 @@ dat_frame = []
 print("Enter run name")
 rname = str(input())
 for t in range(iter):
+    ## making string for the input file name 
     st = '_'
     for i in range(N):
         st = st + str(X_new[i]) + "_"
     original = "PK_Si_tandem_DST_reference.txt"
+    ##creating input file directly in the folder where simulator/CROWM can pick it and simulate it
     f = open(r"C:\Users\Shaurya\Crowm_sims_BO_shaurya\In\PK_Si_tandem" + st + ".txt", "w")
     ct = 0
     for line in open(original):
@@ -125,7 +108,8 @@ for t in range(iter):
     l = str(0)
     f.write(l)
     f.close()
-
+    
+    ##checking if simulator/CROWM returned the file
     file_exists=False
     print("Waiting...for",st, datetime.datetime.now())
     while(file_exists==False):
@@ -137,6 +121,7 @@ for t in range(iter):
         else:
             time.sleep(10)
 
+    ## Copying current value from the output file coming from CROWM/simulator
     ct = 0
     f1 = open(r"C:\Users\Shaurya\Crowm_sims_BO_shaurya\Out\PK_Si_tandem"+st+"_Jsc.txt", "r")
     for line in f1:
@@ -151,28 +136,28 @@ for t in range(iter):
     Current_density_silicon.append(l2)
     Current_density_tandem.append(curr)
 
+    ## Saving both X(thickness combination) and Y (current values- all three) into text files for later
     savetxt("Jsc_perovskite"+rname+".txt",Current_density_perovskite)
     savetxt("Jsc_silicon"+rname+".txt",Current_density_silicon)
     savetxt("Jsc_tandem"+rname+".txt",Current_density_tandem)
     F_train = np.append(F_train, curr)
     X_new_copy = X_new
     dat_frame.append(X_new_copy)
-
     savetxt("df" + rname + ".csv", dat_frame)
     savetxt("F_train" + rname + ".txt", F_train)
+    
+    ##implementing algorithm
     f_max = np.max(F_train)
-
-    # mu = np.zeros((np.prod(df_n['total points'])))
-    # sigma = np.zeros((np.prod(df_n['total points'].to_numpy())))
-    # EI = np.zeros((np.prod(df_n['total points'].to_numpy())))
-      # max of z in the z value set
     dftest, scaler = scan_set_gen()
+    ## GPR fit   ##Refer to sklearn library to understand the arguments sent into the function
     gpr = gp.GaussianProcessRegressor(kernel=None, optimizer="fmin_l_bfgs_b", n_restarts_optimizer=10, alpha=1e-3,
                                       normalize_y=True, random_state=2).fit(df, F_train)
 
 
+    # Predicting mu and sigma
     mu, sigma = gpr.predict(dftest, return_std=True)
-
+    
+    ## Acquisiton function evaluation
     p = (mu - f_max) / (sigma)
     CDF = norm.cdf(p)
     PDF = norm.pdf(p)
@@ -183,18 +168,21 @@ for t in range(iter):
 
     EI[np.where(sigma < 1e-4)] = 0
 
+    # Locating and storing the next query point
     newsample = dftest.iloc[np.argmax(EI)]
 
+    ## Converting preprocessed data back to normal values in order to print on the terminal
     X_new=np.empty(N, dtype=int)
     for i in range(N):
         X_new[i]=scaler[i].inverse_transform((newsample[i]).reshape(-1,1))
+        
     ##appending
-
     df.loc[len(df)] = newsample
 
-    #F_train = np.append(F_train, (F[tuple(X_new[i] for i in range(len(X_new)))]))
     print(curr, "          curr at   ", X_new_copy)
-    if (abs(curr - f_max)/f_max) < 1e-4:
+    
+    ##Checking if maxima has been reached and convergence achieved by making sure each new value of current is 1e-4 close to the previous value continuously 15 times
+    if (abs(curr - f_max)) < 1e-3:
         tick = True
         print("Location         "+"Maxima")
         print(*X_new_copy, "         ", curr)
@@ -208,10 +196,10 @@ for t in range(iter):
         c=0
     if  c==15:
         break
-
+    #notes the maximum value at t iterations 
     maximum_tracker[t] = np.max(F_train)
 
-
+#plots maximum_tracker array vs iteration
 plt.plot(maximum_tracker[0:last_iter], marker='+')
 plt.title("Z_value vs iterations")
 plt.xlabel("Iterations")
